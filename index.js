96469723a698f469159af3152273c37112a18c05
Ghudsanyosief <yohanajeson18@gmail.com>
2026-02-02 02:27:39 +0100

    Test movies site contain right  title

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import { loadMovie, loadMovies } from "./src/movies.js";
-
-const app = express();
-app.use(express.static("."));
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
-
-app.get("/", async (req, res) => {
-  const movies = await loadMovies();
-  res.render("home", { movies });
-});
-
-app.get("/movies/:movieId", async (req, res) => {
-  const movie = await loadMovie(req.params.movieId);
-  res.render("movie", { movie });
-});
+import { app } from "./src/app.js";
 
 //app.use("/static", express.static("./static"));
 
-app.listen(3080);
+app.listen(5080);
